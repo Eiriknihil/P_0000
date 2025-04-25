5972aaaa1b824cc865f702387e1499c466191b1f
Enrique Orihuela <134656348+CaseBroom65@users.noreply.github.com>
2025-04-24 20:49:36 -0600

    main menu

---
+++ P_0000/Assets/Scripts/Menu/MainMenuScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+
+        SceneManager.LoadScene("Level_0");
+
+    }
+
+  
+    public void QuitGame()
+    {
+   
+        Debug.Log("Saliendo del juego...");
+   
+        Application.Quit();
+    }
+}
